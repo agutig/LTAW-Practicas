6dd271417ebb2e2a300b501774f3a4c41dec3d46
Guti <alvarogutierrezg99@gmail.com>
2023-02-15 22:33:51 +0100

    aaa

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,3 +1,27 @@
+
+//Test:  http://127.0.0.1:8080/
+
+
+function print_info_req(req) {
+
+  console.log("");
+  console.log("Mensaje de solicitud");
+  console.log("====================");
+  console.log("Método: " + req.method);
+  console.log("Recurso: " + req.url);
+  console.log("Version: " + req.httpVersion)
+  console.log("Cabeceras: ");
+
+  for (hname in req.headers)
+    console.log(`  * ${hname}: ${req.headers[hname]}`);
+
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);
+  console.log("URL completa: " + myURL.href);
+  console.log("  Ruta: " + myURL.pathname);
+}
+
+
+
 const http = require('http');
 
 //-- Definir el puerto a utilizar
