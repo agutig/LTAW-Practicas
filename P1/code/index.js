622ff4559edb802f0eb3d19702cf9d54bb92892a
Guti <alvarogutierrezg99@gmail.com>
2023-03-02 02:00:23 +0100

    Alma de front

--- P1/code/index.js
+++ P1/code/index.js
@@ -23,3 +23,34 @@ function showOnScroll() {
 }
 
 window.addEventListener('scroll', showOnScroll);
+
+
+var jBalvin = {
+    name: 'J Balvin',
+    coment: 'Lo unico bueno que tienen es el front de la web, no les vuelvo a comprar.',
+    image: 'images/cliente_1.png'
+}
+
+var guillermo = {
+    name: 'Guillermo Del Todo',
+    coment: 'Han escrito mal mi nombre caguen la leche',
+    image: 'images/cliente_2.png'
+}
+
+reviews = [guillermo,jBalvin]
+
+names = document.getElementsByClassName("reviewName");
+coments = document.getElementsByClassName("reviewText");
+images = document.getElementsByClassName("reviewUserImage");
+
+
+index = 0
+
+setInterval(function() {
+    names[0].innerHTML = reviews[index % reviews.length].name
+    coments[0].innerHTML = reviews[index % reviews.length].coment
+    images[0].src = reviews[index % reviews.length].image
+    index += 1
+    console.log("La función timer se ha activado.");
+    console.log(name.innerHTML)
+  }, 5000);
