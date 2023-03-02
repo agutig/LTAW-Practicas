2da7e5df871eb6d613554bb43bd1e41fab35c136
Guti <alvarogutierrezg99@gmail.com>
2023-03-02 01:01:59 +0100

    epa

--- P1/code/index.js
+++ P1/code/index.js
@@ -1,14 +1,17 @@
 
 
-console.log("hey")
+
 
 function showOnScroll() {
+    deleteAnim = document.getElementsByClassName("opacityOnLoad3")
+    for (var i = 0; i < deleteAnim.length; i++) {
+        deleteAnim[i].classList.remove('opacityOnLoad3');
+    }
     var elementsToShow = document.getElementsByClassName("fade");
     for (var i = 0; i < elementsToShow.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = elementsToShow[i].getBoundingClientRect().top;
         elementsToShow[i].style.opacity = 0.2 +(windowHeight - elementTop)/windowHeight
-        console.log(2**(elementTop/windowHeight))
         /*
         if (elementTop > 0 && elementTop < windowHeight) {
             elementsToShow[i].style.opacity = 1
