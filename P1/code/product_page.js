fd0085d1b99d4a206b989ab5f34d1b1affcbe54e
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2023-03-04 21:03:42 +0100

    se va cerrando la p1

--- P1/code/product_page.js
+++ P1/code/product_page.js
@@ -8,15 +8,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         fileListLen = 3
         img_url = image.src.split("/");
         file = img_url[img_url.length - 1].split("_")
-        index = file[file.length - 1].split(".")[0]
+        index = parseInt(file[file.length - 1].split(".")[0])
 
         //Update data
-        index += step
-        index = Math.abs(index % fileListLen)
+        index += parseInt(step)
+        console.log(index)
+        if (index < 0){ index = fileListLen -1}
+        //index = Math.abs(index % fileListLen)
 
         var ppt = document.getElementById("ppt" + String(index));
         ppt.style.textShadow  = "0.75px 0.75px 0 #1b1b1b, -0.75px -0.75px 0 #1b1b1b,  0.75px -0.75px 0 #1b1b1b,   -0.75px 0.75px 0 #1b1b1b"
-        console.log(ppt )
+        
 
         //Load data
         file[file.length -1] = String(index) + ".png"
