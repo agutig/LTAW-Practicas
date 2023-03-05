c3009e015c37cfad7aa309926366f470787ae74b
Guti <alvarogutierrezg99@gmail.com>
2023-03-05 01:49:32 +0100

    Cambio de filosofia

---
+++ P2/tienda.js
@@ -0,0 +1,73 @@
+
+//Test:  http://localhost:9000/
+
+//Modules
+const fs = require('fs');
+const http = require('http');
+
+function print_info_req(req) {
+
+  console.log("");
+  console.log("Mensaje de solicitud");
+  console.log("====================");
+  console.log("Método: " + req.method);
+  console.log("Recurso: " + req.url);
+  console.log("Version: " + req.httpVersion)
+  console.log("Cabeceras: ");
+
+  for (hname in req.headers)
+    console.log(`  * ${hname}: ${req.headers[hname]}`);
+
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);
+  console.log("URL completa: " + myURL.href);
+  console.log("  Ruta: " + myURL.pathname);
+
+  return myURL
+}
+
+
+function OK(res,data){
+
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    //res.setHeader("content-Type" , String(type[0]));
+    res.write(data);
+    res.end();
+    console.log("    200 OK")
+
+}
+
+
+function NOT_OK(res){
+  res.statusCode = 404;
+  res.statusMessage = "Not Found"
+  res.setHeader('Content-Type','text/html');
+  fs.readFile('error.html', (err, data) => { if(!err){
+    res.write(data)
+    res.end();
+    console.log("    Error 404 NOT FOUND")
+  }});
+  
+}
+
+
+const PUERTO = 9000;
+
+const server = http.createServer((req, res) => {
+    
+  url = print_info_req(req)
+  if (req.method == "GET" ){
+
+    if (url.pathname == '/'){
+      fs.readFile('index.html', (err, data) => { if(!err){OK(res,data)}else{NOT_OK(res)}});
+
+    }else{
+      fs.readFile(url.pathname.slice(1,), (err, data) => { if(!err){OK(res,data)}else{NOT_OK(res)}});
+    }
+
+  }
+
+});
+
+server.listen(PUERTO);
+console.log("Servidor activado. Escuchando en puerto: " + PUERTO);
