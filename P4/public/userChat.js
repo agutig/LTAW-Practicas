afa9631ab753f3e5b0206a882d3cb707ef92b2f7
Guti <alvarogutierrezg99@gmail.com>
2023-05-17 22:42:03 +0200

    actu

--- P4/public/userChat.js
+++ P4/public/userChat.js
@@ -7,12 +7,24 @@ input = document.getElementById("inputInput");
 //Send User-server messages
 
 
+function sendMessage(){
+  //Mandar un mensaje SI atado a un evento
+  console.log([USERNAME,input.value])
+  msg = input.value
+  input.value = ""
+  messagesDiv.innerHTML += "<div class='messageClassDiv2'> <span class='userName'>Tú</span> <p class='chatText'>"+msg+"</p>  </div>"
+  socket.emit("message" , [USERNAME,msg])
+}
+
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    sendMessage()
+  }
+});
+
 button.onclick = () => {
-    //Mandar un mensaje SI atado a un evento
-    console.log([USERNAME,input.value])
-    msg = input.value
-    messagesDiv.innerHTML += "<div class='messageClassDiv2' white-space: pre-wrap;> <p >"+msg+"</p>  <div>"
-    socket.emit("message" , [USERNAME,msg]) 
+  sendMessage()
 }
 
 //Manage Server-client responses
@@ -27,11 +39,11 @@ socket.on("disconnect", ()=> {
 
 socket.on("message", (msg)=>{
   console.log(msg)
-  messagesDiv.innerHTML += "<div class='messageClassDiv1' white-space: pre-wrap;> <p>" + msg[0] + "</p> <p>"+ msg[1] +"</p> <div>"
+  messagesDiv.innerHTML += "<div class='messageClassDiv1'> <p class='userName' >" + msg[0] + "</p> <p class='chatText' >"+ msg[1] +"</p> <div>"
 });
 
 socket.on("server", (msg)=>{
   console.log(msg)
-  messagesDiv.innerHTML += "<div class='messageClassDiv3' white-space: pre-wrap;> <p>" + msg + "</p>"
+  messagesDiv.innerHTML += "<div class='messageClassDiv3'> <p class='userName'>Server</p>  <p class='chatText' >" + msg + "</p>"
 });
 
